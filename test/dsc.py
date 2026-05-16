3685f2d624631584acb9cabe8bc94e9dc0c2c742
polybrod <102337762+polybrod@users.noreply.github.com>
2026-05-15 19:34:50 -0700

    Update dsc.py
    capitalize default cycler

--- test/dsc.py
+++ test/dsc.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import savgol_filter
 from scipy.optimize import curve_fit
-from .utils import first_line, default_cycler
+from .utils import first_line, DEFAULT_CYCLER
 
 def readDSC(path, **kwargs):
     '''
